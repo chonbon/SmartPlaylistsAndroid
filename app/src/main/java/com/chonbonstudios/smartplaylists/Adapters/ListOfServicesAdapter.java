de1d50ab8932f4f3109151a9eeebe3c358284ab5
chonbon <austinchoncek@hotmail.com>
2020-05-22 01:30:27 -0400

    Commit to save

--- app/src/main/java/com/chonbonstudios/smartplaylists/Adapters/ListOfServicesAdapter.java
+++ app/src/main/java/com/chonbonstudios/smartplaylists/Adapters/ListOfServicesAdapter.java
@@ -1,51 +1,60 @@
 package com.chonbonstudios.smartplaylists.Adapters;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.chonbonstudios.smartplaylists.ModelData.StreamingServices;
-import com.chonbonstudios.smartplaylists.R;
-
 import java.util.ArrayList;
-
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.chonbonstudios.smartplaylists.ModelData.StreamingServices;
+import com.chonbonstudios.smartplaylists.R;
+
 public class ListOfServicesAdapter extends RecyclerView.Adapter<ListOfServicesAdapter.MyViewHolder> {
     private ArrayList<StreamingServices> mDataset;
+    private OnServiceClick mOnServiceClick;
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ListOfServicesAdapter(ArrayList<StreamingServices> myDataset) {
+    public ListOfServicesAdapter(ArrayList<StreamingServices> myDataset, OnServiceClick onServiceClick) {
         mDataset = myDataset;
+        mOnServiceClick = onServiceClick;
     }
 
 
     //Direct ref to each item in the view
-    static class MyViewHolder extends RecyclerView.ViewHolder {
+    static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView serviceName;
         ConstraintLayout layout;
-        MyViewHolder(LinearLayout v) {
+        OnServiceClick onServiceClick2;
+        MyViewHolder(LinearLayout v,OnServiceClick onServiceClick) {
             super(v);
             serviceName = v.findViewById(R.id.txtServiceListName);
             layout = v.findViewById(R.id.containerList);
+            onServiceClick2 = onServiceClick;
+            v.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            onServiceClick2.onServiceClick(getAdapterPosition());
         }
     }
 
 
 
     //Inflate the view that is the list design
+    @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // create a new view
         LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_services, parent, false);
 
-        MyViewHolder vh = new MyViewHolder(v);
+        MyViewHolder vh = new MyViewHolder(v,mOnServiceClick);
         return vh;
     }
 
@@ -66,5 +75,9 @@ public class ListOfServicesAdapter extends RecyclerView.Adapter<ListOfServicesAd
     @Override
     public int getItemCount() {
         return mDataset.size();
+    }
+
+    public interface OnServiceClick {
+        void onServiceClick(int position);
     }
 }

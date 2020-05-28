ae98c2ca566334c2ae930ae5c88b365fe892e320
chonbon <austinchoncek@hotmail.com>
2020-05-28 12:51:41 -0400

    Switched from implicit grant to long term refresh token on spotify and cleaned some sign in code up
    TODO
    
    add button to the button of the transfer activity to continue flow of transferring after user makes selection
    add settings gear to the top menu to manage settings
    create a settings page with these starting options:
    Services management (shows what is signed in and whats not)
    status of user (pro or free)
    restore purchase button
    About app header
    Developer
    App version

--- app/src/main/java/com/chonbonstudios/smartplaylists/MainActivity.java
+++ app/src/main/java/com/chonbonstudios/smartplaylists/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
 
         client = new OkHttpClient();
         Uri uri = getIntent().getData();
